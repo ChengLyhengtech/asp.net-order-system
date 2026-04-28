b18465b6de059b10a33c5b7a47caa042df589a59
Chey Kimirth <cheykimrith123@gmail>
2026-04-28 17:07:42 +0700

    debug/net0.8

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,16 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using aps.net_order_system.Models; // Import your Models namespace
+using aps.net_order_system.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore; // Import your Models namespace
 
 namespace aps.net_order_system.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<UserModel>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
 
         // This creates the "Users" table in your database
-        public DbSet<UserModel> Users { get; set; }
         public DbSet<CategoriesModel> Categories { get; set; }
         public DbSet<ProductModel> Products { get; set; }
         public DbSet<OrderItemModel> OrderItems { get; set; }
--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -1,17 +1,14 @@
-﻿namespace aps.net_order_system.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace aps.net_order_system.Models
 {
-    public class UserModel
+    public class UserModel:IdentityUser
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
+        [Required]
+        public string FullName { get; set; } = string.Empty;
 
-        // Identity Fields
-        public string Username { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string PhoneNumber { get; set; } = string.Empty;
-
-        // Security
-        public string PasswordHash { get; set; } = string.Empty;
-        // Metadata
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
     }
 }
