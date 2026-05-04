60283e3968ef3bad4d6a95cd72e7d573159eb699
zakk125 <vannzakk@gmail.com>
2026-05-04 13:58:27 +0700

    update main zakk

--- obj/Debug/net8.0/aps.net order-system.AssemblyInfo.cs
+++ obj/Debug/net8.0/aps.net order-system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aps.net order-system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8d76bcb35727cacfdd10defbc3aed1291872bfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ff9e5c437e0d14b6f73dc36b00c44ff410e30be")]
 [assembly: System.Reflection.AssemblyProductAttribute("aps.net order-system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aps.net order-system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
