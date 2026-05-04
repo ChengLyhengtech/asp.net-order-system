f6299ecd0ca1ce03ccb044b597ecab68558c68b3
zakk125 <vannzakk@gmail.com>
2026-05-04 16:52:30 +0700

    avaa

--- obj/Debug/net8.0/aps.net order-system.AssemblyInfo.cs
+++ obj/Debug/net8.0/aps.net order-system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aps.net order-system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c6e603865f63adbd0a83b1d44a1b7e35b112ffc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+066681a6d6edb0a1c3db826d4c903ddea450df09")]
 [assembly: System.Reflection.AssemblyProductAttribute("aps.net order-system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aps.net order-system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
