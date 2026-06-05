bd087a07053b87a79a575b4999c5d8cec595ff01
Galingdu <henglanhzaa09.12.2021@gmail.com>
2026-06-05 14:28:32 +0700

    sdfsa

--- obj/Debug/net8.0/aps.net order-system.AssemblyInfo.cs
+++ obj/Debug/net8.0/aps.net order-system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aps.net order-system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d1eb54f9edc37944deaba0a8d77d275defeacf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93b7f24c3a4be80543dce548acfd2b6c303e3675")]
 [assembly: System.Reflection.AssemblyProductAttribute("aps.net order-system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aps.net order-system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
